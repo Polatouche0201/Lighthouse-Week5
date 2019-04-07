71307429977ff832e30d9fc4aa857ffdfbae9d93
Polatouche0201 <polatouche@outlook.com>
2019-04-07 17:30:34 +0000

    lightcoin_crypto updated and class-parent-child test added.

--- lightcoin_crypto/index.js
+++ lightcoin_crypto/index.js
@@ -2,20 +2,22 @@ let balance = 500.00;
 
 class Account {
 
-  constructor(username) {
-    this.username = username;
-    // Have the account balance start at $0 since that makes more sense.
-    this.balance = 0;
+  constructor() {
+    this.transactions = [];
   }
 
-  // get balance() {
-  //   // Calculate the balance using the transaction objects.
-  //   return this.balance;
-  // }
+  get balance() {
+    // Calculate the balance using the transaction objects.
+    let balance = 0;
+    for(let transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
+  }
 
-  // addTransaction(transaction) {
-  //   this.transactions.push(transaction);
-  // }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 
 }
 
@@ -27,16 +29,22 @@ class Transaction {
   }
 
   commit() {
-    this.account.balance += this.value;
+    if(!this.isAllowed()) {
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
 
-// Strech Work
-  // commit() {
-  //   // Keep track of the time of the transaction
-  //   this.time = new Date();
-  //   // Add the transaction to the account
-  //   this.account.addTransaction(this);
-  // }
+  testChildFun() {
+    this.testFun();
+    //this.testFun2();
+  }
+
+  testParentFun() {
+    console.log("This is a transaction.");
+  }
 
 }
 
@@ -44,6 +52,16 @@ class Deposit extends Transaction {
 
   get value() {
     return this.amount;
+  }
+
+  isAllowed() {
+    // deposits always allowed thanks to capitalism.
+    return true;
+  }
+
+  testFun() {
+    this.testParentFun();
+    console.log("This is a deposit.");
   }
 
 }
@@ -54,17 +72,41 @@ class Withdrawal extends Transaction {
     return -this.amount;
   }
 
+  isAllowed() {
+    // note how it has access to this.account b/c of parent
+    return (this.account.balance - this.amount >= 0);
+  }
+
+  // testFun2() {
+  //   this.testParentFun();
+  //   console.log("This is a withdraw.");
+  // }
+
 }
 
-// DRIVER CODE BELOW
+// DRIVER CODE (yes, keep everything in one file for now... b/c cog load)
+const myAccount = new Account();
 
-const myAccount = new Account('billybob');
+console.log('Starting Account Balance: ', myAccount.balance);
 
-console.log('Starting Balance:', myAccount.balance);
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
 
-const t1 = new Deposit(120.00, myAccount);
-t1.commit();
-const t2 = new Withdrawal(50.00, myAccount);
-t2.commit();
+// console.log('Commit result:', t1.commit());
+// console.log('Account Balance: ', myAccount.balance);
 
-console.log('Ending Balance:', myAccount.balance);
+// console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+t2.testChildFun();
+t1.testChildFun();
+// console.log('Commit result:', t2.commit());
+// console.log('Account Balance: ', myAccount.balance);
+
+// console.log('Withdrawal for 9.99 should be allowed...');
+// const t3 = new Withdrawal(9.99, myAccount);
+// console.log('Commit result:', t3.commit());
+
+// console.log('Ending Account Balance: ', myAccount.balance);
+// console.log("Lookings like I'm broke again");
+
+// console.log('Account Transaction History: ', myAccount.transactions);
