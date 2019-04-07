fd58ff7dd5f1e8b89a931b3ab9f24e2f4a5ea532
Polatouche0201 <polatouche@outlook.com>
2019-04-07 19:45:54 +0000

    sum-all-number-in-array-test Completed

--- sum-all-number-in-array-test/index.js
+++ sum-all-number-in-array-test/index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  if(array.length > 0) {
+    let element = array.shift();
+    if(Array.isArray(element)) {
+      return sumItems(element) + sumItems(array);
+    } else {
+      return element + sumItems(array);
+    }
+  } else {
+    return 0;
+  }
 }
 
 module.exports = sumItems;
