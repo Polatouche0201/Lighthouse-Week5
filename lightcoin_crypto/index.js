6517c1094eb452d94beebdddf93f2354007547d6
Polatouche0201 <polatouche@outlook.com>
2019-04-07 04:25:46 +0000

    lightcoin_crypto Added

---
+++ lightcoin_crypto/index.js
@@ -0,0 +1,70 @@
+let balance = 500.00;
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+
+  // get balance() {
+  //   // Calculate the balance using the transaction objects.
+  //   return this.balance;
+  // }
+
+  // addTransaction(transaction) {
+  //   this.transactions.push(transaction);
+  // }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance += this.value;
+  }
+
+// Strech Work
+  // commit() {
+  //   // Keep track of the time of the transaction
+  //   this.time = new Date();
+  //   // Add the transaction to the account
+  //   this.account.addTransaction(this);
+  // }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+// DRIVER CODE BELOW
+
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
+t1.commit();
+const t2 = new Withdrawal(50.00, myAccount);
+t2.commit();
+
+console.log('Ending Balance:', myAccount.balance);
