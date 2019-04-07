47b7c9ffb66caec4d01b5a3e562a3fe2884690b4
Polatouche0201 <polatouche@outlook.com>
2019-04-07 19:01:34 +0000

    sum-from-a-to-b-test completed

--- sum-from-a-to-b-test/index.js
+++ sum-from-a-to-b-test/index.js
@@ -1,6 +1,15 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if(fromN === toN) {
+    return fromN;
+  } else {
+    if((fromN + 1) === toN) {
+      return fromN + toN;
+    } else {
+      return fromN + toN + sum(fromN + 1,toN - 1);
+    }
+  }
 }
 
 module.exports = sum;
